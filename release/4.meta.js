d6f4628069c05b4850017ec9ee04f18634809e22
gnadgnaoh <gnadgnaoh@users.noreply.github.com>
2024-10-10 02:12:31 +0000

    Bump

---
+++ release/4.meta.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name        Bypass All Shortlinks Manual Captcha
+// @namespace   Violentmonkey Scripts
+// @author      Bloggerpemula
+// @version     92.8
+// @match       *://*/*
+// @grant       GM_setValue
+// @grant       GM_getValue
+// @grant       GM_addStyle
+// @run-at      document-start
+// @connect     nocaptchaai.com
+// @grant       GM_xmlhttpRequest
+// @grant       window.onurlchange
+// @grant       GM_registerMenuCommand
+// @icon        https://i.ibb.co/qgr0H1n/BASS-Blogger-Pemula.png
+// @copyright   2022+, Bloggerpemula (https://openuserjs.org/users/Bloggerpemula)
+// @license     CC-BY-NC-SA-4.0; https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
+// @updateURL  https://raw.githubusercontent.com/gnadgnaoh/Shortlink/release/release/4.meta.js
+// @license     GPL-3.0-or-later; https://www.gnu.org/licenses/gpl-3.0.txt
+// @require     https://update.greasyfork.org/scripts/439099/1203718/MonkeyConfig%20Modern%20Reloaded.js
+// @description Bypass All Shortlinks Sites Automatically Skips Annoying Link Shorteners , Skip AdFly and No Annoying Ads, Directly to Your Destination and now Support Auto Downloading Your Files
+// @exclude /^(https?:\/\/)(.+)?((advertisingexcel|talkforfitness|rsadnetworkinfo|rsinsuranceinfo|rsfinanceinfo|rssoftwareinfo|rshostinginfo|rseducationinfo|gametechreviewer|vegan4k|phineypet|batmanfactor|techedifier|urlhives|linkhives|gsshort|substitutefor|github|freeoseocheck|greenenez|aliyun|reddit|bing|yahoo|wiki-topia|edonmanor|vrtier|whatsapp|gearsadviser|edonmanor|tunebug|menrealitycalc|amazon|ebay|payoneer|paypal|skrill|stripe|tipalti|wise|discord|tokopedia|taobao|taboola|aliexpress|netflix|citigroup|spotify|bankofamerica|hsbc|accounts.youtube|(cloud|mail|translate|analytics|accounts|myaccount|contacts|clients6|developers|payments|pay|ogs|safety|wallet).google|(login|signup|account|officeapps|api|mail|hotmail).live|basketballsavvy|healthyfollicles|hauntingrealm|pluginmixer|boredboard|boardgamechick|healthyfollicles|atlassian|pinterest|twitter|facebook|tiktok|instagram|linkedin).com|(coinsvalue|cookinguide|cryptowidgets|webfreetools|carstopia|makeupguide|carsmania|doubleclick|luckydice).net|(linksfly|shortsfly|urlsfly|wefly|blog24).me|(greasyfork|openuserjs|telegram|wikipedia|lichess).org|misterio.ro|insurancegold.in|coinscap.info|chefknives.expert|(sleeptube|englishwritingsite|documentaryplanet|gsgames).xyz|(shopee|lazada|rakuten|maybank|binance).*|(dana|ovo|bca.co|bri.co|bni.co|bankmandiri.co|desa|(.*).go).id|(.*).(edu|gov))(\/.*)/
+// ==/UserScript==
